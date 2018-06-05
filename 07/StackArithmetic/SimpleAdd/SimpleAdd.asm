7724de5dc752e17aa13ecb4842a1aa9141b463bb
Xi Jin <xi.jin@weightwatchers.com>
2018-06-04 23:36:14 -0400

    finished arithmetic

---
+++ 07/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,25 @@
+// Created by Xi Jin
+// C_PUSH constant 7
+@7
+D=A
+@0
+A=M
+M=D
+@0
+M=M+1
+// C_PUSH constant 8
+@8
+D=A
+@0
+A=M
+M=D
+@0
+M=M+1
+// arithmetic: add
+@0
+M=M-1
+A=M
+D=M
+@0
+A=M-1
+M=M+D
--- 07/VMTranslator/src/com/inwebland/nand2tetris/CodeWriter.java
+++ 07/VMTranslator/src/com/inwebland/nand2tetris/CodeWriter.java
@@ -5,10 +5,14 @@ import java.io.*;
 public class CodeWriter {
     private BufferedWriter writer;
 
+    private int labelCounter = 0;
+    private int variableCounter = 0;
     private static final int SP_LOC = 0;
     private static final int STACK_LOC = 256;
     private static final int HEAP_LOC = 2048;
     private static final int IO_LOC = 16384;
+    private static final int TRUE = -1;
+    private static final int FALSE = 0;
 
     public CodeWriter(File output){
         try {
@@ -45,15 +49,104 @@ public class CodeWriter {
             case "add":
                 writeLine("@" + SP_LOC);
                 writeLine("A=M-1");
-                writeLine("M=M-D");
+                writeLine("M=M+D");
+                break;
             case "sub":
+                writeLine("@" + SP_LOC);
+                writeLine("A=M-1");
+                writeLine("M=M-D");
+                break;
             case "neg":
+                writeLine("@" + SP_LOC);
+                writeLine("M=M+1");
+                writeLine("A=M-1");
+                writeLine("M=0");
+                writeLine("M=M-D");
+                break;
             case "eq":
+                String ifEqualLabel = "IF_EQUAL." + labelCounter;
+                labelCounter++;
+                String endEqualLabel = "IF_END." + labelCounter;
+                labelCounter++;
+
+                writeLine("@" + SP_LOC);
+                writeLine("M=M-1");
+                writeLine("A=M");
+                writeLine("D=M-D");
+                writeLine("@" + ifEqualLabel);
+                writeLine("D;JEQ");
+                writeLine("D=" + FALSE);
+                writeLine("@" + endEqualLabel);
+                writeLine("0;JMP");
+                writeLine("(" + ifEqualLabel + ")");
+                writeLine("D=" + TRUE);
+                writeLine("(" + endEqualLabel + ")");
+                writeLine("@" + SP_LOC);
+                writeLine("M=M+1");
+                writeLine("A=M-1");
+                writeLine("M=D");
+                break;
             case "gt":
+                String ifGreaterLabel = "IF_GREATER." + labelCounter;
+                labelCounter++;
+                String greaterEndEqualLabel = "IF_END." + labelCounter;
+                labelCounter++;
+
+                writeLine("@" + SP_LOC);
+                writeLine("M=M-1");
+                writeLine("A=M");
+                writeLine("D=M-D");
+                writeLine("@" + ifGreaterLabel);
+                writeLine("D;JGT");
+                writeLine("D=" + FALSE);
+                writeLine("@" + greaterEndEqualLabel);
+                writeLine("0;JMP");
+                writeLine("(" + ifGreaterLabel + ")");
+                writeLine("D=" + TRUE);
+                writeLine("(" + greaterEndEqualLabel + ")");
+                writeLine("@" + SP_LOC);
+                writeLine("M=M+1");
+                writeLine("A=M-1");
+                writeLine("M=D");
+                break;
             case "lt":
+                String ifLesserLabel = "IF_LESSER." + labelCounter;
+                labelCounter++;
+                String lesserEndEqualLabel = "IF_END." + labelCounter;
+                labelCounter++;
+
+                writeLine("@" + SP_LOC);
+                writeLine("M=M-1");
+                writeLine("A=M");
+                writeLine("D=M-D");
+                writeLine("@" + ifLesserLabel);
+                writeLine("D;JLT");
+                writeLine("D=" + FALSE);
+                writeLine("@" + lesserEndEqualLabel);
+                writeLine("0;JMP");
+                writeLine("(" + ifLesserLabel + ")");
+                writeLine("D=" + TRUE);
+                writeLine("(" + lesserEndEqualLabel + ")");
+                writeLine("@" + SP_LOC);
+                writeLine("M=M+1");
+                writeLine("A=M-1");
+                writeLine("M=D");
+                break;
             case "and":
+                writeLine("@" + SP_LOC);
+                writeLine("A=M-1");
+                writeLine("M=D&M");
+                break;
             case "or":
+                writeLine("@" + SP_LOC);
+                writeLine("A=M-1");
+                writeLine("M=D|M");
+                break;
             case "not":
+                writeLine("@" + SP_LOC);
+                writeLine("M=M+1");
+                writeLine("A=M-1");
+                writeLine("M=!D");
                 break;
         }
 
